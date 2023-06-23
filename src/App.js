327766d5106ef086ed97a2f51a13fc4f08d67126
dkrmentor <dkrmentor@gmail.com>
2023-06-24 01:15:52 +0500

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FibonacciSphere from './FibonacciSphere'
+
+const App = () => {
+  return (
+    <div>
+      <FibonacciSphere />
+    </div>
+  )
+}
+
+export default App
